73aafb6bf979a3b547d7f91fb3305b55b5eae8b6
smallsweetsweet1 <1007512569@qq.com>
2019-12-06 18:26:12 +0800

    change in schema and  data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,7 +1,7 @@
 set names utf8;
-INSERT INTO `user` VALUES (1, 'name', '111', 'nikename', 'URL', '男', '11', '1');
-INSERT INTO `videoinfo` VALUES (1, 'name', 'type', 'url', 'uploader', '男', 'like', 'collection', 2000-1-1 00:00:00);
-INSERT INTO `comment` VALUES (1, 'videoid', 'name', 'comment', 2000-1-1 00:00:00);
-INSERT INTO `collection` VALUES (1, 'videoid', 'name');
-INSERT INTO `watchinghistory` VALUES (1, 'videoid', 'name');
+INSERT INTO `user_info` VALUES (1, 'name', '111', 'nikename', 'URL', '男', '11', '1');
+INSERT INTO `video_info` VALUES (1, 'name', 'type', 'url', 'uploader', '男', 'like', 'collection', 2000-1-1 00:00:00);
+INSERT INTO `comment` VALUES (1, 'video_id', 'name', 'comment', 2000-1-1 00:00:00);
+INSERT INTO `collection` VALUES (1, 'video_id', 'name');
+INSERT INTO `watchinghistory` VALUES (1, 'video_id', 'name');
 commit;
